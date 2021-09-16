29747a43fd768c98b4e890341d1d1facca5defc3
harish kumar <87074160+harishKumar1307@users.noreply.github.com>
2021-09-16 17:21:17 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "${var.workshop-prefix}-subnet"
   virtual_network_name = "${azurerm_virtual_network.vnet.name}"
   resource_group_name  = "${azurerm_resource_group.hashitraining.name}"
-  address_prefix       = "${var.subnet_prefix}"
+  address_prefixes      = "${var.subnet_prefix}"
 }
 
 resource "azurerm_network_security_group" "vault-sg" {
